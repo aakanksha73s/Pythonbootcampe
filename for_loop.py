8db4318f5225e299e4070b58313e5655f9b659f2
aakanksha73s <singhhridesh1971@gmail.com>
2024-09-18 00:23:43 +0530

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,8 @@
+n = int(input("Enter a value for n: "))
+
+# Calculate the upper limit of the range
+upper_limit = 10 * n
+
+# Use a for loop to iterate through numbers from 1 to 10n
+for i in range(1, upper_limit + 1):
+    print(i)
